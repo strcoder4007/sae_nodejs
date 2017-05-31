2cf7dba7098bfc9e798279a58708b58d14b3c080
Shubham Singh <mynameshubham@gmail.com>
2017-05-31 22:11:46 +0400

    removed bugs

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -16,6 +16,26 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 		.when('/sponsor', {
 			templateUrl: 'views/sponsor.html',
 			controller: 'SponsorController'
+		})
+
+		.when('/about', {
+			templateUrl: 'views/home.html',
+			controller: 'MainController'
+		})
+
+		.when('/teams', {
+			templateUrl: 'views/home.html/#teams',
+			controller: 'MainController'
+		})
+
+		.when('/notice', {
+			templateUrl: 'views/home.html/#notice',
+			controller: 'MainController'
+		})
+
+		.when('/contact', {
+			templateUrl: 'views/home.html/#contact',
+			controller: 'MainController'
 		});
 
 	$locationProvider.html5Mode(true);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,11 +1,13 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 		$('.myslider').slick({
-		  dots: true,
 		  infinite: true,
 		  speed: 300,
+		  autoplay: true,
+		  autoplaySpeed: 3000,
 		  slidesToShow: 1,
 		  adaptiveHeight: true,
-		  autoplay: true,
-		  autoplaySpeed: 2000,
+		  mobileFirst: true,
+		  prevArrow: '<button type="button" class="slick-prev" style = "margin-left: 30px; z-index: 10;">Previous</button>',
+		  nextArrow: '<button type="button" class="slick-next" style = "margin-right: 30px;">Next</button>',
 		});
 });
